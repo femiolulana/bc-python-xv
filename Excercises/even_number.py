21daf3f342ab1781e9e01e6930988c1d1744eb03
femiolulana <oniseikeji@gmail.com>
2016-02-17 05:59:14 +0100

    Updating even_number.py

--- Excercises/even_number.py
+++ Excercises/even_number.py
@@ -1,7 +1,6 @@
 def even_numbers(low, high):
-	even_nos = []
-	for item in range(low, high):
-		if item % 2 == 0:
-			print item
-		even_nos.append(item)
-	print even_nos
+        for i in range(low, high): #looping through the given nos.
+                if i % 2 == 0:
+                        print i    #printing one per line
+		
+even_numbers(2,40)	#quick testing,delete when to be applied to an app
