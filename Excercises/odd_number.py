00c18d9ffd34dec9f511a2cdcc399e3af0b3a056
femiolulana <oniseikeji@gmail.com>
2016-02-17 06:11:16 +0100

    Updating odd_number.py with own code

--- Excercises/odd_number.py
+++ Excercises/odd_number.py
@@ -1,7 +1,6 @@
 def odd_numbers(low, high):
-	odd_nos = []
-	for item in range(low, high):
-		if item % 2 != 0:
-			print item
-		odd_nos.append(item)
-	print odd_nos
+        for i in range(low, high): #looping through the given nos.
+                if i % 2 != 0:
+                        print i    #printing one per line
+		
+odd_numbers(2,40)	#quick testing,delete when to be applied to an app
