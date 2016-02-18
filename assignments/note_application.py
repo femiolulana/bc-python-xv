27e84887de815f503bbb8d7229ff5b008782ea3e
femiolulana <oniseikeji@gmail.com>
2016-02-18 17:23:49 +0100

    update on my note-application

--- assignments/note_application.py
+++ assignments/note_application.py
@@ -1,10 +1,22 @@
 class NotesApplication(object):
 	"""docstring for ClassName"""
-	def __init__(self, author,notes):
+	def __init__(self, author,notes): #constructor for notes
 		self.author=author
 		self.notes=notes
 	def create(self,note_content):
 		self.note_content=[NOTE_CONTENT]
 		notes.append=self.
 	def list(self):
-		print notes
+		pass
+
+	def get(self, note_id):
+		pass
+
+	def search(self, search_text):
+		pass
+
+	def delete(self, note_id):
+		pass
+		
+	def edit(self, note_id, new_content):
+		pass	
