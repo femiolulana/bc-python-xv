4fc0e5531f2891830d3653d53706d2d11f6dae2c
femiolulana <oniseikeji@gmail.com>
2016-02-18 17:19:08 +0100

    notes application assignment

---
+++ assignments/note_application.py
@@ -0,0 +1,10 @@
+class NotesApplication(object):
+	"""docstring for ClassName"""
+	def __init__(self, author,notes):
+		self.author=author
+		self.notes=notes
+	def create(self,note_content):
+		self.note_content=[NOTE_CONTENT]
+		notes.append=self.
+	def list(self):
+		print notes
