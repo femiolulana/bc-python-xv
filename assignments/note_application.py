d7802c356388995ef3216d7a3a2a6fef3fdbbd9a
femiolulana <oniseikeji@gmail.com>
2016-02-19 01:49:10 +0100

    updating note application

--- assignments/note_application.py
+++ assignments/note_application.py
@@ -1,22 +1,34 @@
 class NotesApplication(object):
-	"""docstring for ClassName"""
-	def __init__(self, author,notes): #constructor for notes
-		self.author=author
-		self.notes=notes
-	def create(self,note_content):
-		self.note_content=[NOTE_CONTENT]
-		notes.append=self.
-	def list(self):
-		pass
+  def __init__(self, author):
+    self.author = author
+    self.notes_list = []
+  
+  def create(self, note_content):
+    self.note_content = note_content
+    self.notes_list.append(note_content)
+    return self
+    
+  def list(self):
+    for i in range(len(self.notes_list)):
+      print ('Note ID: {}'.format(i))
+      print ('{}'.format(self.notes_list[i]))
+      print ('By Author {}'.format(self.author))
+  
+  def get(self, note_id):
+    self.content = self.notes_list[note_id]
+    return self.content
 
-	def get(self, note_id):
-		pass
-
-	def search(self, search_text):
-		pass
-
-	def delete(self, note_id):
-		pass
-		
-	def edit(self, note_id, new_content):
-		pass	
+  def search(self, search_text):
+    add = "Showing results for search \"[<"+search_text+">]\""
+    for i in self.notes_list:
+      if search_text in i:
+        note_content = self.notes_list.index(i)
+        add += "\n Note ID: %d \n %s \n By Author  %s"%(note_content, i, self.author)
+    a = str(add)
+    print (a)
+    
+  def delete(self, note_id):
+    del self.notes_list[note_id]
+    
+  def edit(self, note_id, new_content):
+    self.notes_list[note_id] = new_content
